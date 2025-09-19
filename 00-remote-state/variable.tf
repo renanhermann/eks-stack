5648bb1ddfa58d5f93d81d3057e49f3225528ec3
renanhermann <renanhermann@outlook.com>
2025-09-19 19:02:40 -0300

    feat: add remote state configuration variables

--- 00-remote-state/variable.tf
+++ 00-remote-state/variable.tf
@@ -10,3 +10,22 @@ variable "tags" {
     Project   = "eks-stack"
   }
 }
+
+variable "remote_state" {
+  type =object({
+    billing_mode = string
+    hash_key = string
+    attribute = object({
+      name = string
+      type = string
+    })
+  })
+  default = {
+    billing_mode = "PAY_PER_REQUEST"
+    hash_key = "LockID"
+    attribute = {
+      name = "LockID"
+      type = "S"
+    }
+  }
+}
