939e4538ed3d047ebd8f095a29feb2807d6dd0ef
renanhermann <renanhermann@outlook.com>
2025-09-19 20:02:09 -0300

    feat: add public subnets and route table variables

--- 01-network/variable.tf
+++ 01-network/variable.tf
@@ -8,11 +8,31 @@ variable "vpc" {
     cidr_block            = string
     name                  = string
     internet_gateway_name = string
+    public_route_table_name = string
+    public_subnets = list(object({
+      cidr_block = string
+      name = string
+      availability_zone = string
+      map_public_ip_on_launch = bool
+    }))
   })
   default = {
     cidr_block            = "10.0.0.0/24"
     name                  = "eks-vpc"
     internet_gateway_name = "igw"
+    public_route_table_name = "public-route-table"
+    public_subnets = [{
+      name = "eks-public-subnet-1"
+      cidr_block = "10.0.0.0/27"
+      availability_zone = "us-east-1a"
+      map_public_ip_on_launch = true
+    },
+    { 
+      name = "eks-public-subnet-2"
+      cidr_block = "10.0.0.64/27"
+      availability_zone = "us-east-1b"
+      map_public_ip_on_launch = true
+    }]
   }
 }
 
