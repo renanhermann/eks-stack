8654f5d1d1160cecedd4d3bf7623fb58e647d53b
renanhermann <renanhermann@outlook.com>
2025-09-19 21:38:11 -0300

    feat: add private subnets

---
+++ 01-network/vpc.private-subnets.tf
@@ -0,0 +1,11 @@
+resource "aws_subnet" "privates" {
+  count = length(var.vpc.private_subnets)
+
+  vpc_id                  = aws_vpc.this.id
+  cidr_block              = var.vpc.private_subnets[count.index].cidr_block
+  availability_zone       = var.vpc.private_subnets[count.index].availability_zone
+  map_public_ip_on_launch = var.vpc.private_subnets[count.index].map_public_ip_on_launch
+
+  tags       = merge({ Name = "${var.vpc.name}-${var.vpc.private_subnets[count.index].name}" }, var.tags)
+  depends_on = [aws_vpc.this]
+}   
