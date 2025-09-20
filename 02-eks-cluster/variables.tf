bd51b4f98ca69e8596575c8cd46391bf7b69eac3
renanhermann <renanhermann@outlook.com>
2025-09-19 23:57:50 -0300

    fix: variables

--- 02-eks-cluster/variables.tf
+++ 02-eks-cluster/variables.tf
@@ -20,7 +20,17 @@ variable "eks_cluster" {
     access_config = object({
       authentication_mode = string
     })
-    subnet_ids = list(string)
+    node_group = object({
+      name = string
+      instance_types = list(string)
+      capacity_type = string
+      ami_type = string
+      scaling_config = object({
+        desired_size = number
+        max_size = number
+        min_size = number
+      })
+    })
   })
   default = {
     name = "eks-stack-cluster"
@@ -30,6 +40,16 @@ variable "eks_cluster" {
     access_config = {
       authentication_mode = "API_AND_CONFIG_MAP"
     }
-    subnet_ids = []
+    node_group = {
+      name = "eks-stack-node-group"
+      instance_types = ["t3.medium"]
+      capacity_type = "ON_DEMAND"
+      ami_type = "AL2023_x86_64_STANDARD"
+      scaling_config = {
+        desired_size = 1
+        max_size = 3
+        min_size = 1
+      }
+    }
   }
 }
