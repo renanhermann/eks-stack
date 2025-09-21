2256e4ea48930fa3711d514a55497b61b47f36a4
renanhermann <renanhermann@outlook.com>
2025-09-20 22:48:06 -0300

    chore: locals change

--- 02-eks-cluster/locals.tf
+++ 02-eks-cluster/locals.tf
@@ -1,4 +1,5 @@
 locals {
-  bash_user_arn = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:user/renan.hermann"
+  bash_user_arn    = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:user/renan.hermann"
   console_user_arn = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:user/console"
+  eks_oidc_url     = replace(aws_eks_cluster.this.identity[0].oidc[0].issuer, "https://", "")
 }
